7ce7fa11c1d93a4f5c9ba8e405114e6826903a03
Mr-Taufik <taozk4417@gmail.com>
2024-12-11 20:39:36 +0800

    Update and rename bubble sort.c to Bubble Sort.c

--- Bubble Sort.c
+++ Bubble Sort.c
@@ -9,10 +9,14 @@
 
 
 #include <stdio.h>
-
+//这里定义了两个不返回的函数，接受两个参数，接收数组和长度
 void bubbleSort(int arr[], int n);
 void printArray(int arr[], int size);
-
+//这里定义数组的时候还可以用
+// void bubbleSort(int *arr, int n);
+// void printArray(int *arr, int size);
+//在C语言中，数组名在大多数表达式中会退化为指向其第一个元素的指针。
+//因此，您只需要传递 arr 或 &arr[0] 即可，它们都是 int * 类型，符合函数的参数要求。
 int main()
 {
 	int arr[] = { 64, 34, 25, 12, 22 ,11, 90 };
@@ -31,21 +35,21 @@ void bubbleSort(int arr[], int n)
 	int temp;
 	int swapped;
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n - 1; i++)  //这里是外层循环，n个数字排序外层需要排序n-1次
 	{
 		swapped = 0;
 
-		for (j = 0; j < n - i - 1; j++)
+		for (j = 0; j < n - i - 1; j++)//这里是内层循环，n个数字内层循环的次数是n-1-i次
 		{
 			if (arr[j] > arr[j + 1])
 			{
 				temp = arr[j];
-				arr[i] = arr[j + 1];
+				arr[j] = arr[j + 1];
 				arr[j + 1] = temp;
 				swapped = 1;
 			}
 		}
-		if (swapped == 0)
+		if (swapped == 0)//swapped 变量在这段代码中用于提前退出循环
 			break;
 	}
 }
